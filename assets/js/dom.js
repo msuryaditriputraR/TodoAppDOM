8d70ffce4eec104d521d832b4b73b1b602d7c61b
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-02 11:59:43 +0800

    add makeToDo Function and show it in container

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,8 +1,32 @@
+const UNCOMPLETED_LIST_TODO_ID = 'todos';
+
 const addToDo = () => {
+    const uncompletedListTodo = document.getElementById(
+        UNCOMPLETED_LIST_TODO_ID
+    );
+
     const title = document.getElementById('title').value;
     const timeStamp = document.getElementById('date').value;
 
-    console.log(`${title} harus selesai sebelum ${timeStamp}`);
+    const todo = makeToDo(title, timeStamp);
+    uncompletedListTodo.append(todo);
+};
+
+const makeToDo = (title, timeStamp) => {
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = title;
+    const textTimeStamp = document.createElement('p');
+    textTimeStamp.innerText = timeStamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textTimeStamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+
+    return container;
 };
 
 export { addToDo };
